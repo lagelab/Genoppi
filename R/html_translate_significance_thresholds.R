0351c26ca763bbf9d0744b14d94f545e793feb7c
frhl <frederik.heymann@gmail.com>
2020-04-28 14:59:15 +1000

    removed non-ASCII chars

--- R/html_translate_significance_thresholds.R
+++ R/html_translate_significance_thresholds.R
@@ -13,17 +13,17 @@
 html_translate_significance_thresholds <- function(fc, fc_dir, sig_type, fdr_thresh, pval_thresh){
   
   # track significance type and threshold
-  sig_type = ifelse(sig_type == 'fdr', 'FDR', paste(italics('P'),'-value'))
+  sig_type = ifelse(sig_type == 'fdr', 'FDR', '<i>P</i>-value')
   sig_value = ifelse(sig_type == 'FDR', fdr_thresh, pval_thresh)
-  fc_sign = ifelse(fc_dir, '<', '≥')
-  region_le <- c(paste0(sig_type,"≤", sig_value))
+  fc_sign = ifelse(fc_dir, '<', '&ge;')
+  region_le <- c(paste0(sig_type,"&le;", sig_value))
   region_g <- c(paste0(sig_type,">", sig_value))
   sig = list(sig=region_le, insig=region_g, sig_type=sig_type, sig_value=sig_value)
   
   # track logfc direction and threshold
-  if (fc_dir == 'negative') {fc_sig = paste0("log", subscript("2"), "FC< ", -fc); fc_insig =  paste0("log", subscript("2"), "FC≥ ", -fc)}
-  if (fc_dir == 'positive') {fc_sig = paste0("log", subscript("2"), "FC≥ ", fc); fc_insig =  paste0("log", subscript("2"), "FC< ", fc)}
-  if (fc_dir == 'both') {fc_sig = paste0("|log", subscript("2"), "FC|≥ ", fc); fc_insig = paste0("|log", subscript("2"), "FC|< ", fc) }
+  if (fc_dir == 'negative') {fc_sig = paste("log<sub>2</sub>FC&lt;", -fc); fc_insig =  paste("log<sub>2</sub>FC&ge;", -fc)}
+  if (fc_dir == 'positive') {fc_sig = paste("log<sub>2</sub>FC&ge;", fc); fc_insig =  paste("log<sub>2</sub>FC&lt;", fc)}
+  if (fc_dir == 'both') {fc_sig = paste("|log<sub>2</sub>FC|&ge;", fc); fc_insig = paste("|log<sub>2</sub>FC|&lt;", fc) }
   fc = list(sig=fc_sig, insig=fc_insig)
   
   return(list(sig=sig, fc=fc))
